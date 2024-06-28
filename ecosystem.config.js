63f331b30cf6ed27392f5ec9e3a3786f6bd92306
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-06-28 15:10:48 -0700

    changes: 6-28-24

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,24 @@
 module.exports = {
-    apps: [
-      {
-        name: 'backend',
-        script: 'backend/server.js',
-        watch: true,
-        env: {
-          NODE_ENV: 'production',
-          PORT: 3000,
-        },
+  apps: [
+    {
+      name: 'backend',
+      script: 'backend/server.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
       },
-      {
-        name: 'frontend',
-        script: 'venv/bin/python',
-        args: 'frontend/app.py',
-        interpreter: 'none',
-        watch: true,
-        env: {
-          FLASK_ENV: 'production',
-          PORT: 5000,
-        },
+    },
+    {
+      name: 'frontend',
+      script: 'venv/bin/python',
+      args: 'frontend/app.py',
+      interpreter: 'none',
+      watch: true,
+      env: {
+        FLASK_ENV: 'production',
+        PORT: 5000,
       },
-    ],
-  };
-  
+    },
+  ],
+};
